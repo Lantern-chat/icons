cfdca4df121291fdcf6a1a10a1e2710ac84acceb
novacrazy <novacrazy@gmail.com>
2022-02-22 11:25:57 -0600

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,74 @@
+export const HomeIcon = "HomeIcon";
+export const SunIcon = "SunIcon";
+export const MoonIcon = "MoonIcon";
+
+export const MobilePhoneIcon = "MobilePhoneIcon";
+
+export const MenuIcon = "MenuIcon";
+export const UsersIcon = "UsersIcon";
+export const HashIcon = "HashIcon";
+
+export const SmileyHalfIcon = "SmileyHalfIcon";
+
+export const BalloonIcon = "BalloonIcon";
+export const BalloonsIcon = "BalloonsIcon";
+
+export const ChevronDownIcon = "ChevronDownIcon";
+
+export const SendIcon = "SendIcon";
+export const PlusIcon = "PlusIcon";
+
+export const SaveIcon = "SaveIcon";
+
+export const PencilIcon = "PencilIcon";
+export const PushPinIcon = "PushPinIcon";
+export const ArrowThinRightIcon = "ArrowThinRightIcon";
+
+export const FamilyIcon = "FamilyIcon";
+
+export const PasteIcon = "PasteIcon";
+
+export const TrashIcon = "TrashIcon";
+export const TrashOpenIcon = "TrashOpenIcon";
+export const ClipboardIcon = "ClipboardIcon";
+export const CopyIcon = "CopyIcon";
+export const ChatMessageIcon = "ChatMessageIcon";
+export const TriangleIcon = "TriangleIcon";
+
+export const TriangleAlertIcon = "TriangleAlertIcon";
+export const LockIcon = "LockIcon";
+
+export const CrownIcon = "CrownIcon";
+
+export const CogwheelIcon = "CogwheelIcon";
+export const SpeakerIcon = "SpeakerIcon";
+export const SpeakerDeafIcon = "SpeakerDeafIcon";
+export const MicrophoneIcon = "MicrophoneIcon";
+export const MicrophoneMuteIcon = "MicrophoneMuteIcon";
+
+export const MenuCloseIcon = "MenuCloseIcon";
+
+export const CloseIcon = "CloseIcon";
+export const FullscreenOffIcon = "FullscreenOffIcon";
+export const FullscreenOnIcon = "FullscreenOnIcon";
+
+export const LogoutIcon = "LogoutIcon";
+
+export const CircleEmptyInfoIcon = "CircleEmptyInfoIcon";
+
+export const PlainTextFileIcon = "PlainTextFileIcon";
+export const RichTextFileIcon = "RichTextFileIcon";
+export const AudioFileIcon = "AudioFileIcon";
+export const VideoFileIcon = "VideoFileIcon";
+export const ImageFileIcon = "ImageFileIcon";
+export const SpreadsheetFileIcon = "SpreadsheetFileIcon";
+export const DatabaseFileIcon = "DatabaseFileIcon";
+export const ProgramFileIcon = "ProgramFileIcon";
+export const TerminalFileIcon = "TerminalFileIcon";
+export const ScriptFileIcon = "ScriptFileIcon";
+export const PresentationFileIcon = "PresentationFileIcon";
+export const UnknownFileIcon = "UnknownFileIcon";
+export const CodeFileIcon = "CodeFileIcon";
+export const ShieldFileIcon = "ShieldFileIcon";
+export const KeyFileIcon = "KeyFileIcon";
+export const ZipFileIcon = "ZipFileIcon";
