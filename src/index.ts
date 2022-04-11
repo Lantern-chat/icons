4befd1ca978b25385013035d7c92368dc7fa5ca9
novacrazy <novacrazy@gmail.com>
2022-04-11 14:24:38 -0500

    Update with IDs and single-sources

--- src/index.ts
+++ src/index.ts
@@ -1,76 +1,155 @@
 export const enum Icons {
-    HomeIcon,
-    SunIcon,
-    MoonIcon,
+    HomeIcon = "home",
+    SunIcon = "sun",
+    MoonIcon = "moon",
+    TranslateIcon = "translate",
 
-    MobilePhoneIcon,
+    MobilePhoneIcon = "mobile-phone",
 
-    MenuIcon,
-    UsersIcon,
-    HashIcon,
+    MenuIcon = "menu",
+    UsersIcon = "users",
+    HashIcon = "hash",
 
-    SmileyHalfIcon,
+    SmileyHalfIcon = "smiley-half",
 
-    BalloonIcon,
-    BalloonsIcon,
+    BalloonIcon = "balloon",
+    BalloonsIcon = "balloons",
 
-    ChevronDownIcon,
+    ChevronDownIcon = "chevron-down",
 
-    SendIcon,
-    PlusIcon,
+    SendIcon = "send",
+    PlusIcon = "plus",
 
-    SaveIcon,
+    SaveIcon = "save",
 
-    PencilIcon,
-    PushPinIcon,
-    ArrowThinRightIcon,
+    PencilIcon = "pencil",
+    PushPinIcon = "push-pin",
+    ArrowThinRightIcon = "arrow-thin-right",
 
-    FamilyIcon,
+    FamilyIcon = "family",
 
-    PasteIcon,
+    PasteIcon = "paste",
 
-    TrashIcon,
-    TrashOpenIcon,
-    ClipboardIcon,
-    CopyIcon,
-    ChatMessageIcon,
-    TriangleIcon,
+    TrashIcon = "trash",
+    TrashOpenIcon = "trash-open",
+    ClipboardIcon = "clipboard",
+    CopyIcon = "copy",
+    ChatMessageIcon = "chat-message",
+    TriangleIcon = "triangle",
 
-    TriangleAlertIcon,
-    LockIcon,
+    TriangleAlertIcon = "triangle-alert",
+    LockIcon = "lock",
 
-    CrownIcon,
+    CrownIcon = "crown",
 
-    CogwheelIcon,
-    SpeakerIcon,
-    SpeakerDeafIcon,
-    MicrophoneIcon,
-    MicrophoneMuteIcon,
+    CogwheelIcon = "cogwheel",
+    SpeakerIcon = "speaker",
+    SpeakerDeafIcon = "speaker-deaf",
+    MicrophoneIcon = "microphone",
+    MicrophoneMuteIcon = "microphone-mute",
 
-    MenuCloseIcon,
+    MenuCloseIcon = "menu-close",
 
-    CloseIcon,
-    FullscreenOffIcon,
-    FullscreenOnIcon,
+    CloseIcon = "close",
+    FullscreenOffIcon = "fullscreen-off",
+    FullscreenOnIcon = "fullscreen-on",
 
-    LogoutIcon,
+    LogoutIcon = "logout",
 
-    CircleEmptyInfoIcon,
+    CircleEmptyInfoIcon = "circle-empty-info",
 
-    PlainTextFileIcon,
-    RichTextFileIcon,
-    AudioFileIcon,
-    VideoFileIcon,
-    ImageFileIcon,
-    SpreadsheetFileIcon,
-    DatabaseFileIcon,
-    ProgramFileIcon,
-    TerminalFileIcon,
-    ScriptFileIcon,
-    PresentationFileIcon,
-    UnknownFileIcon,
-    CodeFileIcon,
-    ShieldFileIcon,
-    KeyFileIcon,
-    ZipFileIcon,
+    PlainTextFileIcon = "plain-text-file",
+    RichTextFileIcon = "rich-text-file",
+    AudioFileIcon = "audio-file",
+    VideoFileIcon = "video-file",
+    ImageFileIcon = "image-file",
+    SpreadsheetFileIcon = "spreadsheet-file",
+    DatabaseFileIcon = "database-file",
+    ProgramFileIcon = "program-file",
+    TerminalFileIcon = "terminal-file",
+    ScriptFileIcon = "script-file",
+    PresentationFileIcon = "presentation-file",
+    UnknownFileIcon = "unknown-file",
+    CodeFileIcon = "code-file",
+    ShieldFileIcon = "shield-file",
+    KeyFileIcon = "key-file",
+    ZipFileIcon = "zip-file",
 }
+
+export const HomeIcon = "HomeIcon";
+export const SunIcon = "SunIcon";
+export const MoonIcon = "MoonIcon";
+export const TranslateIcon = "TranslateIcon";
+
+export const MobilePhoneIcon = "MobilePhoneIcon";
+
+export const MenuIcon = "MenuIcon";
+export const UsersIcon = "UsersIcon";
+export const HashIcon = "HashIcon";
+
+export const SmileyHalfIcon = "SmileyHalfIcon";
+
+export const BalloonIcon = "BalloonIcon";
+export const BalloonsIcon = "BalloonsIcon";
+
+export const ChevronDownIcon = "ChevronDownIcon";
+
+export const SendIcon = "SendIcon";
+export const PlusIcon = "PlusIcon";
+
+export const SaveIcon = "SaveIcon";
+
+export const PencilIcon = "PencilIcon";
+export const PushPinIcon = "PushPinIcon";
+export const ArrowThinRightIcon = "ArrowThinRightIcon";
+
+export const FamilyIcon = "FamilyIcon";
+
+export const PasteIcon = "PasteIcon";
+
+export const TrashIcon = "TrashIcon";
+export const TrashOpenIcon = "TrashOpenIcon";
+export const ClipboardIcon = "ClipboardIcon";
+export const CopyIcon = "CopyIcon";
+export const ChatMessageIcon = "ChatMessageIcon";
+export const TriangleIcon = "TriangleIcon";
+
+export const TriangleAlertIcon = "TriangleAlertIcon";
+export const LockIcon = "LockIcon";
+
+export const CrownIcon = "CrownIcon";
+
+export const CogwheelIcon = "CogwheelIcon";
+export const SpeakerIcon = "SpeakerIcon";
+export const SpeakerDeafIcon = "SpeakerDeafIcon";
+export const MicrophoneIcon = "MicrophoneIcon";
+export const MicrophoneMuteIcon = "MicrophoneMuteIcon";
+
+export const MenuCloseIcon = "MenuCloseIcon";
+
+export const CloseIcon = "CloseIcon";
+export const FullscreenOffIcon = "FullscreenOffIcon";
+export const FullscreenOnIcon = "FullscreenOnIcon";
+
+export const LogoutIcon = "LogoutIcon";
+
+export const CircleEmptyInfoIcon = "CircleEmptyInfoIcon";
+
+export const PlainTextFileIcon = "PlainTextFileIcon";
+export const RichTextFileIcon = "RichTextFileIcon";
+export const AudioFileIcon = "AudioFileIcon";
+export const VideoFileIcon = "VideoFileIcon";
+export const ImageFileIcon = "ImageFileIcon";
+export const SpreadsheetFileIcon = "SpreadsheetFileIcon";
+export const DatabaseFileIcon = "DatabaseFileIcon";
+export const ProgramFileIcon = "ProgramFileIcon";
+export const TerminalFileIcon = "TerminalFileIcon";
+export const ScriptFileIcon = "ScriptFileIcon";
+export const PresentationFileIcon = "PresentationFileIcon";
+export const UnknownFileIcon = "UnknownFileIcon";
+export const CodeFileIcon = "CodeFileIcon";
+export const ShieldFileIcon = "ShieldFileIcon";
+export const KeyFileIcon = "KeyFileIcon";
+export const ZipFileIcon = "ZipFileIcon";
+
+export const path = "";
